f3fb534dd8b955a92c1c10c6d4a817ea2e0c4cef
Karl Stich <stich.karl@gmail.com>
2021-12-08 08:09:33 +0000

    install ansible on master

--- environments/nodes-from-mig/scripts/master.sh
+++ environments/nodes-from-mig/scripts/master.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
+
+if [ -z $(which ansible) ]; then 
+  sudo apt-get update  
+  sudo apt-get install --yes software-properties-common  
+  sudo apt-add-repository --yes --update ppa:ansible/ansible  
+  sudo apt-get install --yes ansible  
+else
+  sudo apt-get update 
+  sudo apt-get install --yes ansible
+fi
+
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.21.5+k3s2" sh -s - \
 	--write-kubeconfig-mode 644 \
 	--token "${token}" \
