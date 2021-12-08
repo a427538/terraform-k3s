6fb28613b609131ea4091036e276aa18554d929d
Karl Stich <stich.karl@gmail.com>
2021-12-08 08:40:40 +0000

    install ansible

--- environments/dev/scripts/master.sh
+++ environments/dev/scripts/master.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+
+apt-get update  
+apt-get install --yes software-properties-common  
+apt-add-repository --yes --update ppa:ansible/ansible  
+apt-get install --yes ansible  
+
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.21.5+k3s2" sh -s - \
 	--write-kubeconfig-mode 644 \
 	--token "${token}" \
