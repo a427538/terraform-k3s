88fe8ecde32370a42e01f44557b7610289880bbb
Karl Stich <stich.karl@gmail.com>
2021-12-17 17:42:51 +0000

    next hop ip

--- modules/routing/variables.tf
+++ modules/routing/variables.tf
@@ -2,6 +2,5 @@ variable "subnet" {}
 variable "next_hop_ip" {
   # the value doesn't matter; we're just using this variable
   # to propagate dependencies.
-  type    = any
-  default = []
+  type    = string
 }
