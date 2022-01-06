7605f5287a4fecbb1741137c799e8a33795e99c4
Karl Stich <stich.karl@gmail.com>
2022-01-06 14:57:47 +0000

    new k3s version v1.22.5+k3s1

--- environments/dev/scripts/master.sh
+++ environments/dev/scripts/master.sh
@@ -5,12 +5,12 @@ apt-get install --yes vim mc aptitude python software-properties-common net-tool
 apt-add-repository --yes --update ppa:ansible/ansible  
 apt-get install --yes ansible 
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.21.5+k3s2" sh -s - \
-	--flannel-backend none \
-    --disable-network-policy \
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.22.5+k3s1" sh -s - \
     --write-kubeconfig-mode 644 \
 	--token "${token}" \
 	--tls-san "${external_lb_ip_address}" \
+	--flannel-backend none \
+    --disable-network-policy \    
 	--disable traefik
 
 # Enable ip forwarding and nat
--- environments/dev/scripts/worker.sh
+++ environments/dev/scripts/worker.sh
@@ -5,7 +5,7 @@ apt-get install --yes vim mc aptitude python software-properties-common net-tool
 apt-add-repository --yes --update ppa:ansible/ansible  
 apt-get install --yes ansible  
 
-curl -sfL https://get.k3s.io | K3S_TOKEN="${token}" INSTALL_K3S_VERSION="v1.21.5+k3s2" K3S_URL="https://${server_address}:6443" sh -s -
+curl -sfL https://get.k3s.io | K3S_TOKEN="${token}" INSTALL_K3S_VERSION="v1.22.5+k3s1" K3S_URL="https://${server_address}:6443" sh -s -
 
 git clone https://github.com/a427538/terraform-k3s.git
 cd terraform-k3s && git checkout "${branch}"
